ff892d1a41a6cee9d60abe0a166a826d23293d91
gabrielapalhari-droid <gabriela.palhari@escola.pr.gov.br>
2026-05-19 12:32:16 -0300

    4

---
+++ index.js
@@ -0,0 +1,18 @@
+//Lista materiais
+
+let materiais = ['Caderno', 'Lápis', 'Borracha'];
+materiais.push('Caneta', 'Régua');
+//console.log(materiais);
+
+//Lista nomes
+let nomes = ["Ana", "Carlos", "João", "Maria"]
+//console.log('A aluna', nomes[0], 'foi retirada da lista'),
+nomes.shift();
+//console.log(nomes);
+
+//Lista alimentos
+let alimentos = ["Arroz", "Feijão", "Macarrão", "Leite", "Açúcar", "Café"];
+let comida = alimentos.slice(2, 5);
+console.log(alimentos);
+console.log(comida);
+
