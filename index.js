a1bbffe5656d41559259bced9f8269b7be12f2f5
gabrielapalhari-droid <gabriela.palhari@escola.pr.gov.br>
2026-05-20 10:59:31 -0300

    2

--- index.js
+++ index.js
@@ -4,15 +4,39 @@ let materiais = ['Caderno', 'Lápis', 'Borracha'];
 materiais.push('Caneta', 'Régua');
 //console.log(materiais);
 
+
 //Lista nomes
 let nomes = ["Ana", "Carlos", "João", "Maria"]
 //console.log('A aluna', nomes[0], 'foi retirada da lista'),
 nomes.shift();
 //console.log(nomes);
 
+
 //Lista alimentos
 let alimentos = ["Arroz", "Feijão", "Macarrão", "Leite", "Açúcar", "Café"];
 let comida = alimentos.slice(2, 5);
-console.log(alimentos);
-console.log(comida);
+//console.log(alimentos);
+//console.log(comida);
 
+
+//Lista loja
+let loja = ["Mouse", "Teclado", "Monitor", "Impressora"];
+loja[2] = "Notebook";
+//console.log(loja);
+
+//Lista mista 
+let frutas = ["Maçã", "Banana"];
+let verduras = ["alface", "Cenoura"]
+let combinada = frutas.concat(verduras);
+//console.log(frutas);
+//console.log(verduras);
+//console.log(combinada);
+
+
+//Lista notas
+let notas = [
+    [8,7,9],
+    [10,6,8],
+    [7,9,10]
+    ]
+console.log('Primeiro aluno',notas[1]);
